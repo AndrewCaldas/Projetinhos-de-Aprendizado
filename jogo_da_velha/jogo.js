2cfb95d38ca0b96d62c2754aba4a8c4d3085b3ac
Andrew Caldas <andrew.caldas@redfox.tech>
2019-06-07 17:55:44 -0300

    add jogo_da_velha

--- jogo_da_velha/jogo.js
+++ jogo_da_velha/jogo.js
@@ -2,7 +2,29 @@
 
     $(document).ready(function (){
         $('#iniciar').click(function (){
-            alert("teste")
+
+            let jogador1 = $('#jogador-1').val();
+            let jogador2 = $('#jogador-2').val(); 
+
+            if (jogador1 == '') {
+                alert("Nome do jogador 1 não inserido");
+                return false;
+            }
+
+            if (jogador2 == '') {
+                alert("Nome do jogador 2 não inserido");
+                return false;
+            }
+
+            $('#nome-jogador-1').html(jogador1)
+            $('#nome-jogador-2').html(jogador2)
+
+            // ocultar campo e mostrar palco de jogo
+            $('#pagina_inicial').hide();
+            $('#palco_jogo').show();
+
+            // alert($('#jogador-1').val());
+            // alert($('#jogador-2').val());
         })
     })
 
