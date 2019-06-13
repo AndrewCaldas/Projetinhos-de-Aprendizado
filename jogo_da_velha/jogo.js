596e2fa834538a2835517368b8acc51b9ee6fbda
Andrew Caldas <andrew.caldas@redfox.tech>
2019-06-13 16:43:23 -0300

    fix: ajusting

--- jogo_da_velha/jogo.js
+++ jogo_da_velha/jogo.js
@@ -1,6 +1,24 @@
     var rodada = 1;
     var matriz_jogo = Array(3);
 
+    matriz_jogo['a'] = Array(3);
+    matriz_jogo['b'] = Array(3);
+    matriz_jogo['c'] = Array(3);
+
+    // Copulação das arrays
+
+    matriz_jogo['a'][1] = 0;
+    matriz_jogo['a'][2] = 0;
+    matriz_jogo['a'][3] = 0;
+
+    matriz_jogo['b'][1] = 0;
+    matriz_jogo['b'][2] = 0;
+    matriz_jogo['b'][3] = 0;
+
+    matriz_jogo['c'][1] = 0;
+    matriz_jogo['c'][2] = 0;
+    matriz_jogo['c'][3] = 0;
+
     $(document).ready(function (){
         $('#iniciar').click(function (){
 
@@ -37,14 +55,54 @@
                 var ponto = 0;
 
                 if((rodada % 2) == 1){
+                    // alert("jogador 1")
                     icone = 'url("imagens/marcacao_1.png")';
                     ponto = -1;
                 } else{
+                    // alert("jogador 2")
+                    icone = 'url("imagens/marcacao_2.png")';
                     ponto = 1;
                 }
-
+                // alert(rodada)
                 rodada ++;
+
+                $('#'+id).css('background-image', icone);
+
+                let linha_coluna = id.split('-')
+
+                matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
             }
+
+            function verifica() {
+                let pontos = 0;
+                for (let i = 0; i <= 3; i++) {
+                    pontos = pontos + matriz_jogo['a'][i];
+                }
+                ganhador(pontos)
+
+                pontos = 0;
+                for (let i = 0; i <= 3; i++) {
+                    pontos = pontos + matriz_jogo['b'][i];
+                }
+                ganhador(pontos)
+
+                pontos = 0;
+                for (let i = 0; i <= 3; i++) {
+                    pontos = pontos + matriz_jogo['c'][i];
+                }
+                ganhador(pontos)
+            }
+
+            function ganhador(pontos) {
+                if(pontos == -3) {
+                    alert("Jogador numero 1 ganhou!")
+                } else if (pontos == 3){
+                    alert("Jogador numero 2 ganhou!")
+                }
+            }
+
+            
+
         })
     })
 
